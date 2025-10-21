b95f3385226e55fda2c04e97e8d104eb93b66628
GustavoV <gustoloco81@gmail.com>
2025-10-21 14:41:52 -0400

    images fix

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,14 +3,14 @@ export const projects = [
     id: 1,
     title: 'Saint Cat',
     description: "Site da minha futura empresa de criação de livros, jogos, filmes e músicas.",
-    image: 'src/assets/images/saintcat.png',
+    image: 'public/assets/images/saintcat.png',
     link: 'https://saintcat.vercel.app',
   },
   {
     id: 2,
     title: 'Buscador de Músicas',
     description: 'Um site que permite buscar músicas e ouvir trechos delas utilizando o id do iTunes.',
-    image: 'src/assets/images/bmusica.png',
+    image: 'public/assets/images/bmusica.png',
     link: 'https://buscadormusicas.vercel.app',
   },
 
