14c1151648ef485c457fb38d58c0eaefe5a6526d
GustavoV <gustoloco81@gmail.com>
2025-10-21 22:23:10 -0400

    readme update

--- src/data/projects.js
+++ src/data/projects.js
@@ -13,5 +13,18 @@ export const projects = [
     image: '/bmusica.png',
     link: 'https://buscadormusicas.vercel.app',
   },
-
+  {
+    id: 3,
+    title: 'Tabuada simples',
+    description: 'Um aplicativo simples para gerar a tabuada de qualquer número de 1 a 10.',
+    image: '/tabuada.png',
+    link: 'https://gvtabuada.vercel.app',
+  },
+  {
+    id: 4,
+    title: 'Relogio Digital',
+    description: 'Um relógio digital feito com JavaScript que muda de cor conforme o período do dia.',
+    image: '/relogio.png',
+    link: 'https://gvrelogio.vercel.app',
+  }
 ]
